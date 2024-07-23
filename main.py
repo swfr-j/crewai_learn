b865c7d5563bc1e7011511ff3d9d1788ecad8665
swfr-j <130271263+swfr-j@users.noreply.github.com>
2024-07-23 16:43:50 +0530

    project completion

--- main.py
+++ main.py
@@ -1,44 +1,64 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from crewai import Crew, Process
 from langchain_community.llms.ollama import Ollama
-from agents import AINewsLetterAgents
-from tasks import AINewsLetterTasks
-from file_io import save_markdown
+from langchain_community.tools import DuckDuckGoSearchResults
+from crewai import Crew, Agent, Task, Process
+from crewai_tools import tool
 
-agents = AINewsLetterAgents()
-tasks = AINewsLetterTasks()
+llm = Ollama(model="openhermes")
 
-llm = Ollama(
-    model="openhermes:latest",
+
+@tool("Search the internet using duckduckgo")
+def search(query: str):
+    """Search the internet using duckduckgo"""
+    search_results = DuckDuckGoSearchResults().run(query)
+    return search_results
+
+
+# Define agents with specific roles and tools
+researcher = Agent(
+    role="Senior Research Analyst",
+    goal="Discover innovative AI technologies",
+    backstory="""You're a senior research analyst at a large company.
+        You're responsible for analyzing data and providing insights
+        to the business.
+        You're currently working on a project to analyze the
+        trends and innovations in the space of artificial intelligence.""",
+    tools=[search],
+    llm=llm,
 )
 
-# setting up agents
-editor = agents.editor_agent()
-news_fetcher = agents.news_fetcher_agent()
-news_analyzer = agents.news_analyzer_agent()
-newsletter_compiler = agents.newsletter_compiler_agent()
-
-# setting up tasks
-fetch_news_task = tasks.fetch_news_task(news_fetcher)
-analyzed_news_task = tasks.analyze_news_task(news_analyzer, [fetch_news_task])
-compiled_newsletter_task = tasks.compile_newsletter_task(
-    newsletter_compiler, [analyzed_news_task], callback_function=save_markdown
+writer = Agent(
+    role="Content Writer",
+    goal="Write engaging articles on AI discoveries",
+    backstory="""You're a senior writer at a large company.
+        You're responsible for creating content to the business.
+        You're currently working on a project to write about trends
+        and innovations in the space of AI for your next meeting.""",
+    verbose=True,
+    llm=llm,
 )
 
-# setting up crew
+# Create tasks for the agents
+research_task = Task(
+    description="Identify breakthrough AI technologies",
+    agent=researcher,
+    expected_output="A bullet list summary of the top 5 most important AI news",
+)
+write_article_task = Task(
+    description="Draft an article on the latest AI technologies",
+    agent=writer,
+    expected_output="3 paragraph blog post on the latest AI technologies",
+)
+
 crew = Crew(
-    agents=[editor, news_fetcher, news_analyzer, newsletter_compiler],
-    tasks=[fetch_news_task, analyzed_news_task, compiled_newsletter_task],
-    process=Process.hierarchical,
-    verbose=2,
+    agents=[researcher, writer],
+    tasks=[research_task, write_article_task],
+    processes=Process.sequential,
+    full_output=True,
+    verbose=True,
     manager_llm=llm,
 )
 
-# running the crew
-results = crew.kickoff()
 
-print("Crew Work Results:")
-print(results)
+result = crew.kickoff()
+
+print(result)
